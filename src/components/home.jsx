5e7f2c3a25cab671e3fa3c4891a06628461c3b8c
Salome Lambermont <salome.lambermont@gmail.com>
2018-07-16 14:56:23 +0100

    Create home page with react-router-dom

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is my site!</h2>
+        <p>yeaaaaaah react</p>
+      </div>
+    );
+  }
+}
+
+export default Home
