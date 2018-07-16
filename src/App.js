29b723c76b276da5ebec1ed895b81c18979a4b5b
Salome Lambermont <salome.lambermont@gmail.com>
2018-07-16 15:48:06 +0100

    Add about route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
 import Home from './components/home.jsx'
+import About from './components/about.jsx'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div><Route exact path='/' component={Home}/></div>
+        <div>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+        </div>
       </Router>
     );
   }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h2>This is my site!</h2>
-        <p>yeaaaaaah react</p>
+        <h2>This is my home page</h2>
+        <Link to='/about'>ABOUT</Link>
       </div>
     );
   }
